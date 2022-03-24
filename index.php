842c0bff7a6b9294a725c2a221c01d34e50be981
Thiago Leite <thiagoleites@outlook.com.br>
2022-03-23 22:43:23 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,25 +2,4 @@
 
 require ('config.php');
 
-$user = new Methods();
-$us = new Pacientes();
-$ex = new Exames();
-
-
-foreach ($user->retornaPacientes() as $row){
-    echo $row['nome_p'] . ' data de nascimento - '. $row['nasc_p'] . '<br>';
-}
-var_dump($user);
-
-echo __LINE__;
-echo '<hr>';
-
-foreach ($ex->retornaExame() as $e){
-    echo $e['nome_e'] . '<br>';
-}
-
-
-
-// $con = new Conn();
-// $con = $con->getConn();
-// var_dump($con);
+header("Location: dashboard.php");
